5941a380773e4a3bec736b657f5f6bace5aab303
Zahra Hanifi <zahrahanifi98@gmail.com>
2024-07-20 23:59:16 +0330

    Add search functionality

--- components/Search.vue
+++ components/Search.vue
@@ -1,15 +1,20 @@
 <script setup>
+const model = defineModel()
 </script>
 
 <template>
   <div class="py-4 px-3 linear-bg">
     <div class="p-3 bg-[#2C2E30] max-w-[1200px] mx-auto rounded-lg flex gap-x-2">
       <input
+          v-model="model"
           class="bg-[#3B3D3F] flex-grow p-3 rounded text-white text-sm outline-none"
           placeholder="Search for characters..."
       />
 
-      <button class="bg-[#DC2626] rounded px-4 text-white text-sm font-medium flex items-center gap-x-2">
+      <button
+        class="bg-[#DC2626] rounded px-4 text-white text-sm font-medium flex items-center gap-x-2"
+        @click="$emit('search')"
+      >
         <img src="/images/search.png" class="min-w-[14px] w-full" />
 
         <span class="hidden sm:block">Search</span>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <script setup>
-import {getMarvelAuthParams} from '~/utils/marvel'
-import {ref, onBeforeMount, watch} from 'vue'
-import Skeleton from "~/components/Skeleton.vue";
+import { getMarvelAuthParams } from '~/utils/marvel'
+import { ref, onBeforeMount, watch } from 'vue'
 import { useRuntimeConfig } from '#imports'
 
 const page = ref(1)
@@ -10,19 +9,25 @@ const {ts, apikey, hash} = getMarvelAuthParams()
 const characters = ref(null)
 const loading = ref(false)
 const config = useRuntimeConfig()
+const searchTerm = ref('')
 
 async function fetchData() {
     loading.value = true
 
+    let params = {
+        ts,
+        apikey,
+        hash,
+        offset: (page.value - 1) * itemsPerPage.value,
+        limit: itemsPerPage.value,
+    }
+    if (searchTerm.value) {
+        params = {...params, name: searchTerm.value}
+    }
+
     try {
         const response = await $fetch(`${config.public.baseURL}/characters`, {
-            params: {
-                ts,
-                apikey,
-                hash,
-                offset: (page.value - 1) * itemsPerPage.value,
-                limit: itemsPerPage.value,
-            }
+            params: params
         })
         loading.value = false
         return response.data
@@ -44,15 +49,24 @@ onBeforeMount(async () => {
 watch(page, async () => {
     characters.value = await fetchData()
 })
+
+async function search() {
+    if (!searchTerm.value) return
+    characters.value = await fetchData()
+}
 </script>
 
 <template>
     <div class="bg-[#2c2e30] min-h-screen pb-10">
-        <Search />
+        <Search v-model="searchTerm" @search="search" />
 
         <div class="max-w-[1200px] mx-auto py-8 px-3">
-            <div v-if="loading || !characters?.results.length" class="flex flex-wrap">
+            <div v-if="loading" class="flex flex-wrap">
                 <Skeleton/>
+            </div>
+
+            <div v-else-if="!characters?.results.length" class="text-3xl text-center text-white flex flex-wrap justify-center">
+                No information found
             </div>
 
             <template v-else>
